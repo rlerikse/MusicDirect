786e3c25375bf17c3483b99a419a7a8755548402
rlerikse <rlerikse@mtu.edu>
2017-10-15 21:37:30 -0400

    first commit

---
+++ app/src/main/java/com/example/bobby/musicdirect/Playlist.java
@@ -0,0 +1,13 @@
+package com.example.bobby.musicdirect;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Playlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+    }
+}
